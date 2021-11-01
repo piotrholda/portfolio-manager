a07ddf3e8bae6bb9b7a12c68459aea1e1b024e2a
Piotr Holda <pholda@gmail.com>
2021-11-01 17:51:26 +0100

    Move services to the bash script.

--- docker/h2.sh
+++ docker/h2.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 java \
-   ${JAVA_OPTIONS} \
-   -cp /var/lib/h2/h2.jar \
-   org.h2.tools.Server \
-   -web -webDaemon -webAllowOthers -webPort 8082 \
-   -tcp -tcpAllowOthers -tcpPort 9082 \
-   -baseDir /usr/lib/h2 \
-   ${H2_OPTIONS}
+  ${JAVA_OPTIONS} \
+  -cp /var/lib/h2/h2.jar \
+  org.h2.tools.Server \
+  -web -webDaemon -webAllowOthers -webPort 8082 \
+  -tcp -tcpAllowOthers -tcpPort 9082 \
+  -baseDir /usr/lib/h2 \
+  ${H2_OPTIONS} &
+java \
+  -Dspring.profiles.active=prod \
+  -jar \
+  /opt/app/portfolio-manager-0.0.1-SNAPSHOT.jar &
