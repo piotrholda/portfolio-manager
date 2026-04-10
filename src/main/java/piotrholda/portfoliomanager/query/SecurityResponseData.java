e7cf8b802cc02a5a596a201490e3d66f0911df3a
phold <pholda@gmail.com>
2026-04-10 19:11:48 +0200

    Use Jacksonized annotation

--- src/main/java/piotrholda/portfoliomanager/query/SecurityResponseData.java
+++ src/main/java/piotrholda/portfoliomanager/query/SecurityResponseData.java
@@ -1,10 +1,14 @@
 package piotrholda.portfoliomanager.query;
 
+import lombok.Builder;
 import lombok.Value;
+import lombok.extern.jackson.Jacksonized;
 import piotrholda.portfoliomanager.SecurityType;
 import piotrholda.portfoliomanager.Ticker;
 
 @Value
+@Jacksonized
+@Builder
 public class SecurityResponseData {
     String securityId;
     SecurityType type;
